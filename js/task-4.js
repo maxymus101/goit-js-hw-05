97801e881bb64c5c25591edce42b627cb9d53635
Max Akimov <maxymus2828@gmail.com>
2025-02-02 20:59:01 +0200

    update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@
 const getTotalBalanceByGender = (users, gender) =>
   users
     .filter((user) => user.gender === gender)
-    .reduce((total, user) => ((total += user.balance), 0));
+    .reduce((total, user) => total + user.balance, 0);
 
 const clients = [
   {
